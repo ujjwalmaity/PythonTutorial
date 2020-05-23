fa6de25952742c00333774334f13137f566daf91
ujjwal <ujjwalmaity98@gmail.com>
2020-05-23 09:36:03 +0530

    Basic Python Concepts

---
+++ 1_basic_python_concepts.py
@@ -0,0 +1,48 @@
+# Hello World
+print('Hello World!')
+
+# Mathematical Operation
+200 + 300
+200 - 300
+75 * 32
+6.0 * 3
+5 * 6 * (5 + 89)
+500 / 100
+500 // 100
+2 ** 3
+49 ** (1 / 2)
+7 % 3
+
+# String
+'Hello World'
+"Hello World"
+'He\'s a good guy'
+"He's a good guy"
+'He is a "good" guy'
+"""He's is a "good" guy"""
+
+# Input & Output
+input('Please enter a value: ')
+print('Hello mike \nWelcome to the club')
+
+# Perform operation on string
+'Hello' + ' World' + '!'
+'5' + '5'
+5 + 5
+# 'cd' + 5      -->This is wrong
+'rob ' * 3
+# 'cd' * '3'    -->This is wrong
+
+# Variable
+a = 10
+b = 'ABC'
+del a
+
+# In place operator
+age = 22
+age += 1
+age -= 1
+age *= 2
+age /= 2
+a = 'Hello'
+a += 'World!'
