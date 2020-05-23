5dcd872bc98f47eadb903e45ad9f67c7a18fe347
ujjwal <ujjwalmaity98@gmail.com>
2020-05-23 13:44:35 +0530

    String formatting, String function & Numeric function

---
+++ 14_string_formatting.py
@@ -0,0 +1,6 @@
+numbers = [23, 5, 2020]
+newString = "Date: {0}/{1}/{2}".format(numbers[0], numbers[1], numbers[2])
+print(newString)
+
+a = "{x}/{y}".format(x=100, y=200)
+print(a)
---
+++ 15_string_function.py
@@ -0,0 +1,10 @@
+print(",".join(['Apple', 'Banana', 'Mango']))
+print(":".join(['Apple', 'Banana', 'Mango']))
+
+print("Hello There!".replace("There", "World"))
+
+print('This is a string.'.startswith('This'))
+print('This is a string.'.endswith('string.'))
+
+print('This is a string.'.upper())
+print('This is a string.'.lower())
